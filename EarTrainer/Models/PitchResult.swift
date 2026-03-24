a522e23b60cf43dc46b44b92144d3dbd77433590
John O'Leary <jgoleary@Mac.ht.home>
2026-03-24 15:01:59 -0700

    fix: use Double range literals in PitchResult.score switch

--- EarTrainer/Models/PitchResult.swift
+++ EarTrainer/Models/PitchResult.swift
@@ -9,8 +9,8 @@ enum PitchResult: Equatable {
         case .onPitch:              return 1.0
         case .sharp(let c), .flat(let c):
             switch abs(c) {
-            case 26...50:           return 0.6
-            case 51...100:          return 0.3
+            case 26.0...50.0:       return 0.6
+            case 51.0...100.0:      return 0.3
             default:                return 0.0
             }
         case .undetected:           return 0.0
