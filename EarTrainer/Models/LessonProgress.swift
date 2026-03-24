2db51d19d6bc9b540250edcb87f0d393b91219ca
John O'Leary <jgoleary@Mac.ht.home>
2026-03-24 15:38:25 -0700

    fix: remove dead didSet clamp from LessonProgress (clamping is in ProgressStore)

--- EarTrainer/Models/LessonProgress.swift
+++ EarTrainer/Models/LessonProgress.swift
@@ -1,6 +1,6 @@
+import Foundation
+
 struct LessonProgress: Codable {
     let lessonID: String
-    var bestStars: Int {      // clamped to 0–3
-        didSet { bestStars = max(0, min(3, bestStars)) }
-    }
+    var bestStars: Int
 }
